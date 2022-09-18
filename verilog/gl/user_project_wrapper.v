3d29a23dd3745c29b947ad9515d759614d1f8b0e
Vinay Rayapati <110079631+vinayrayapati@users.noreply.github.com>
2022-09-18 21:57:01 +0530

    Update user_project_wrapper.v

--- verilog/gl/user_project_wrapper.v
+++ verilog/gl/user_project_wrapper.v
@@ -54,7 +54,7 @@ module user_project_wrapper (user_clock2,
  input [3:0] wbs_sel_i;
 
 
-iiitb_rv32i mprj (.vccd1(vccd1),
+iiitb_rv32i iiitb_rv32i (.vccd1(vccd1),
     .vssd1(vssd1),
     .clk(wb_clk_i),
     .RN(wb_rst_i),
